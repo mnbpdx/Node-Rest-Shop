a739b23f0e12531ee52b8f30e653e91cf9ddb1f9
Mark N. Beebe <mbeebe08@gmail.com>
2021-01-31 09:22:59 -0800

    Add image handling

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,12 +2,41 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const app = require("../../app");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // accept a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  }
+  //reject a file
+  else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter
+});
 
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
-    .select("name price _id")
+    .select("name price _id productImage")
     .exec()
     .then((docs) => {
       const response = {
@@ -16,6 +45,7 @@ router.get("/", (req, res, next) => {
           return {
             name: doc.name,
             price: doc.price,
+            productImage: doc.productImage,
             _id: doc._id,
             request: {
               type: "GET",
@@ -36,11 +66,13 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", upload.single("productImage"), (req, res, next) => {
+  console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
+    productImage: req.file.path
   });
   product
     .save()
@@ -70,7 +102,7 @@ router.post("/", (req, res, next) => {
 router.get("/:productID", (req, res, next) => {
   const id = req.params.productID;
   Product.findById(id)
-    .select("name price _id")
+    .select("name price _id productImage")
     .exec()
     .then((doc) => {
       console.log(doc);
@@ -78,10 +110,10 @@ router.get("/:productID", (req, res, next) => {
         res.status(200).json({
           product: doc,
           request: {
-            type: 'GET',
-            description: 'Get all products',
-            url: 'http://localhost/products'
-          }
+            type: "GET",
+            description: "Get all products",
+            url: "http://localhost/products",
+          },
         });
       } else {
         res.status(404).json({
@@ -106,11 +138,11 @@ router.patch("/:productID", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(200).json({
-        message: 'Product Updated',
+        message: "Product Updated",
         request: {
-          type: 'GET',
-          url: 'http://localhost:3000/products/' + id
-        }
+          type: "GET",
+          url: "http://localhost:3000/products/" + id,
+        },
       });
     })
     .catch((err) => {
@@ -125,12 +157,12 @@ router.delete("/:productID", (req, res, next) => {
     .exec()
     .then((result) => {
       res.status(200).json({
-        message: 'Product deleted',
+        message: "Product deleted",
         request: {
-          type: 'POST',
-          url: 'http://localhost:3000/products',
-          body: { name: 'String', price: 'Number'}
-        }
+          type: "POST",
+          url: "http://localhost:3000/products",
+          body: { name: "String", price: "Number" },
+        },
       });
     })
     .catch((err) => {
