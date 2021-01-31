7c08e77656dbc5fd318220c99fd6872771ab1a6a
Mark N. Beebe <mbeebe08@gmail.com>
2021-01-31 13:43:11 -0800

    Add token generation

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use("/user", userRoutes);
 
 // Handles requests that don't correspond to routes
 app.use((req, res, next) => {
-  const error = new Error("Not found");
+  const error = new Error("Route Not found");
   error.status = 404;
   next(error);
 });
